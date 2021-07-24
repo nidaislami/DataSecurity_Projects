7d8fc5cc728ed049391f6f41bf820c95d6c5df9f
Njomza Rexhepi <75095687+NjomzaRexhepi@users.noreply.github.com>
2021-07-24 07:12:16 +0200

    Add files via upload

---
+++ Detyra2/VerifyToken.cs
@@ -0,0 +1,40 @@
+﻿using JWT;
+using JWT.Serializers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCP_Klienti
+{
+    class VerifyToken
+    {
+        private static RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+        private static string publicKey = rsa.ToXmlString(false);
+        public String verifyToken(string token)
+        {
+            try
+            {
+                IJsonSerializer serializer = new JsonNetSerializer();
+                IDateTimeProvider provider = new UtcDateTimeProvider();
+                IJwtValidator validator = new JwtValidator(serializer, provider);
+                IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
+                IJwtDecoder decoder = new JwtDecoder(serializer, validator, urlEncoder);
+
+                var json = decoder.Decode(token, publicKey, verify: false);
+         
+                return json;
+            }
+            catch (TokenExpiredException)
+            {
+                return "Token has expired";
+            }
+            catch (SignatureVerificationException)
+            {
+                return "Token has invalid signature";
+            }
+        }
+    }
+}

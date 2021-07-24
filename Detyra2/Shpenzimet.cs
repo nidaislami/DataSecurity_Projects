1cc291d4f5c9447238e739dc1af405a394319bbd
Njomza Rexhepi <75095687+NjomzaRexhepi@users.noreply.github.com>
2021-07-24 07:11:10 +0200

    Shpenzimet

---
+++ Detyra2/Shpenzimet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server_TCP
+{
+    class Shpenzimet
+    {
+        public string userId { get; set; }
+        public string Llojifatures { get; set; }
+        public decimal viti { get; set; }
+
+        public decimal muaji { get; set; }
+
+        public decimal vleraneeuro { get; set; }
+    }
+}
+
+
